3a18782ed9e0480643608a93467e7ff0783eab1e
Parmi89 <parmi_89@hotmail.com>
2023-09-25 01:21:06 -0300

    Se agrego vista en detalle.html y ventana modal del boton comprar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+//Capturara el evento del click en comprar
+const verCarrito = document.getElementById("verCarrito");
+let carrito = [];
 document.addEventListener("DOMContentLoaded", () => {
     const productosContainer = document.getElementById("productos");
 
@@ -25,19 +28,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 const detalleButton = document.createElement("button");
                 detalleButton.textContent = "Detalle";
-                detalleButton.className = "boton-detalle"
+                detalleButton.className = "boton-verde";
                 detalleButton.addEventListener("click", () => {
-                mostrarDetalle(producto, index);
+                    mostrarDetalle(producto, index);
                 });
-                
+
+                let comprar = document.createElement("button");
+                comprar.innerText = "Comprar";
+                comprar.className = "boton-gris";
+
                 //MOSTRANDO ELEMENTOS EN HTML
                 productoDiv.appendChild(imagen);
                 productoDiv.appendChild(nombre);
                 productoDiv.appendChild(detalle);
                 productoDiv.appendChild(precio);
                 productoDiv.appendChild(detalleButton);
+                productoDiv.appendChild(comprar);
 
                 productosContainer.appendChild(productoDiv);
+
+                //funcionalidad en boton COMPRAR
+                comprar.addEventListener("click", () => {
+                    carrito.push({
+                        id: producto.id,
+                        nombre: producto.nombre,
+                        detalle: producto.detalle,
+                        img: producto.img,
+                        precio: producto.precio,
+                    });
+                    console.log(carrito);
+                });
             });
         })
         .catch((error) => {
@@ -59,4 +79,42 @@ document.addEventListener("DOMContentLoaded", () => {
         )}&imagen=${encodeURIComponent(producto.img)}`;
         window.location.href = `detalles.html?${queryString}`;
     }
+});
+//creando ventana modal
+verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title"> Productos seleccionados </h1>
+    `;
+    modalContainer.append(modalHeader);
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "X";
+    modalbutton.className = "modal-header-button";
+    //cerrar ventana modal
+    modalbutton.addEventListener("click",() => {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = `
+      <img src= "${product.img}">
+      <h1>${product.nombre} </h1> 
+      <h2>$ ${product.precio} </h2>
+      `;
+        modalContainer.append(carritoContent);
+    });
+    //"prod" representa cada producto
+    const total = carrito.reduce((acumulador, prod) => acumulador + prod.precio, 0);
+
+    const totalCompra = document.createElement("div");
+    totalCompra.className = "total-content";
+    totalCompra.innerHTML = `Total a pagar: $ ${total} `;
+    modalContainer.append(totalCompra);
 });
--- js/detalles.js
+++ js/detalles.js
@@ -12,34 +12,53 @@ document.addEventListener('DOMContentLoaded', () => {
     const imagen = urlParams.get('imagen');
 
     if (id) {
-        const detalleId = document.createElement('h4');
+        const detalleId = document.createElement('h5');
         detalleId.textContent = `Codigo: ${id}`;
 
-        const detalleNombre = document.createElement('h2');
+        const detalleNombre = document.createElement('h1');
         detalleNombre.textContent = `${nombre}`;
 
         const detalleDetalle = document.createElement('p');
-        detalleDetalle.textContent = `Detalle: ${detalle}`;
+        detalleDetalle.textContent =`Detalle:${detalle}`;
 
         const detalleDescripcion = document.createElement('p');
         detalleDescripcion.textContent = `Descripcion: ${descripcion}`;
 
-        const detallePrecio = document.createElement('p');
+        const detallePrecio = document.createElement('h4');
         detallePrecio.textContent = `Precio: $ ${precio}`;
 
         const detalleImagen = document.createElement('img');
         detalleImagen.src = imagen;
         detalleImagen.alt = nombre;
 
-        detalleContainer.appendChild(detalleId);    
+        const botonRedireccionar = document.createElement("button");
+        botonRedireccionar.textContent = "Volver";
+        botonRedireccionar.className = "boton-verde"
+        botonRedireccionar.addEventListener("click", function () {
+        window.location.href = "index.html";
+        });
+
         detalleContainer.appendChild(detalleImagen);
+        detalleContainer.appendChild(detalleId);  
         detalleContainer.appendChild(detalleNombre);
         detalleContainer.appendChild(detalleDetalle);
         detalleContainer.appendChild(detalleDescripcion);
         detalleContainer.appendChild(detallePrecio);
+        detalleContainer.appendChild(botonRedireccionar);
     } else {
         const mensajeError = document.createElement('p');
         mensajeError.textContent = 'No se encontraron detalles del producto.';
+        
+        const botonRedireccionar = document.createElement("button");
+        botonRedireccionar.textContent = "Volver";
+        botonRedireccionar.className = "boton-verde"
+        botonRedireccionar.addEventListener("click", function () {
+        window.location.href = "index.html";
+        });
+        
         detalleContainer.appendChild(mensajeError);
+        detalleContainer.appendChild(botonRedireccionar);
+        
     }
+    
 });
